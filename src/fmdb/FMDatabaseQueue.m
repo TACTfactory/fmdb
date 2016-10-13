718c768cb9f12c6bddf753541ca6e5b865e06bc2
Erwan Le Huitouze <erwan.lehuitouze@tactfactory.com>
2016-10-13 12:06:05 +0200

    Log warning on open resultSet only in DEBUG mode

--- src/fmdb/FMDatabaseQueue.m
+++ src/fmdb/FMDatabaseQueue.m
@@ -166,17 +166,16 @@ static const void * const kDispatchQueueSpecificKey = &kDispatchQueueSpecificKey
         FMDatabase *db = [self database];
         block(db);
         
+#if defined(DEBUG) && DEBUG
         if ([db hasOpenResultSets]) {
             NSLog(@"Warning: there is at least one open result set around after performing [FMDatabaseQueue inDatabase:]");
-            
-#if defined(DEBUG) && DEBUG
             NSSet *openSetCopy = FMDBReturnAutoreleased([[db valueForKey:@"_openResultSets"] copy]);
             for (NSValue *rsInWrappedInATastyValueMeal in openSetCopy) {
                 FMResultSet *rs = (FMResultSet *)[rsInWrappedInATastyValueMeal pointerValue];
                 NSLog(@"query: '%@'", [rs query]);
             }
-#endif
         }
+#endif
     });
     
     FMDBRelease(self);
